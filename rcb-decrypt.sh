c00a9c84f7547a623e699d5c520ec02fe5aa3e69
Vladimir Botka <vbotka@gmail.com>
2015-11-04 23:18:49 +0100

    Directory for decrypted files will be created with permissions 0700

--- rcb-decrypt.sh
+++ rcb-decrypt.sh
@@ -35,32 +35,32 @@ printf "$(date) [OK] *** Dencryption from $SRC to $DST started\n" >> $RCB_LOG
 
 if [ -d "$DST" ]; then
     if [ $DST_DEL == "true" ]; then
-	if ((find $DST -mindepth 1 -maxdepth 1 -exec rm -r {} \;) >$RCB_LOG_TEMP_ENC 2>&1); then
-	    printf "$(date) [OK] files in $DST deleted \n" >> $RCB_LOG
+	if ((find $DST -mindepth 1 -maxdepth 1 -exec rm -r {} \;) > $RCB_LOG_TEMP_ENC 2>&1); then
+	    printf "$(date) [OK] Files in $DST deleted\n" >> $RCB_LOG
 	else
-	    printf "$(date) [ERR] Can't delete files in $DST/*\n" >> $RCB_LOG
-	    cat $RCB_LOG_TEMP | $MAIL -s "[ERR] $RCB_HOST decryption of backup failed on deleting files in $DST" $RCB_EMAIL
+	    printf "$(date) [ERR] Cant delete files in $DST\n" >> $RCB_LOG
+	    cat $RCB_LOG_TEMP | $MAIL -s "[ERR] $RCB_HOST Cant delete files in $DST" $RCB_EMAIL
 	    rm $RCB_LOG_TEMP
 	    exit 1
 	fi
     fi
 else
-    if  (mkdir -p $DST >$RCB_LOG_TEMP 2>&1); then
+    if (mkdir -p $DST && chmod $RCB_DEC_MODE $DST > $RCB_LOG_TEMP 2>&1); then
 	printf "$(date) [OK] $DST created \n" >> $RCB_LOG
     else
-	printf "$(date) [ERR] can't create $DST\n" >> $RCB_LOG
-	cat $RCB_LOG_TEMP | $MAIL -s "[ERR] $RCB_HOST mkdir -p $DST failed" $RCB_EMAIL
+	printf "$(date) [ERR] Cant create $DST\n" >> $RCB_LOG
+	cat $RCB_LOG_TEMP | $MAIL -s "[ERR] $RCB_HOST Cant create $DST" $RCB_EMAIL
 	rm $RCB_LOG_TEMP
 	exit 1
     fi
 fi
 
-if ($RSYNCRYPTO $RSYNCRYPTO_PARAM_D -d -r $SRC $DST $RCB_KEYS $RCB_CRT >$RCB_LOG_TEMP 2>&1); then
+if ($RSYNCRYPTO $RSYNCRYPTO_PARAM_D -d -r $SRC $DST $RCB_KEYS $RCB_CRT > $RCB_LOG_TEMP 2>&1); then
     printf "$(date) [OK] *** Decryption from $SRC to $DST finished\n" >> $RCB_LOG
 else
-    printf "$(date) [ERR] *** Decryption from $SRC to $DST finished with error\n" >> $RCB_LOG
+    printf "$(date) [ERR] *** Decryption from $SRC to $DST failed\n" >> $RCB_LOG
     cat $RCB_LOG_TEMP >> $RCB_LOG
-    cat $RCB_LOG_TEMP | $MAIL -s "[ERR] $RCB_HOST Decryption from $SRC to $DST finished with error" $RCB_EMAIL
+    cat $RCB_LOG_TEMP | $MAIL -s "[ERR] $RCB_HOST Decryption from $SRC to $DST failed" $RCB_EMAIL
     rm $RCB_LOG_TEMP
     exit 1
 fi
