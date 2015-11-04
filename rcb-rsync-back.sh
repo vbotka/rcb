e01eee35a3c3105d09c2ac3b5b389ee8aba34325
Vladimir Botka <vbotka@gmail.com>
2015-11-05 00:49:10 +0100

    Option --delete destination before rsync added

--- rcb-rsync-back.sh
+++ rcb-rsync-back.sh
@@ -2,23 +2,29 @@
 
 source /usr/local/etc/rcb.conf
 
-USAGE="$(basename "$0") [-h|--help] [-l|--link] -- rsync from backup
+USAGE="$(basename "$0") [-h|--help] [-l|--link] [-d|--delete] -- rsync from backup
 where:
-    -h --help show this help text
-    -l --link links the origin directory instead of rsync"
+    -h --help     show this help text
+    -l --link     links the origin directory instead of rsync
+    -d --delete   delete the destination defore rsync
 
 LINK="false"
+DELETE="false"
 for i in "$@"; do
     case $i in
-	-h*|--help*)
+	-h|--help)
 	    echo "$USAGE"
 	    exit
 	    ;;
-	-l=*|--link=*)
+	-l|--link)
 	    LINK="true"
+	    ;;
+	-d|--delete)
+	    DELETE="true"
 	    ;;
 	--default)
 	    LINK="false"
+	    DELETE="false"
 	    ;;
 	*)
 	    # unknown option
@@ -30,17 +36,30 @@ SRC="$BCK_USER@$BCK_HOST:$BCK_DST"
 DST="$RCB_ENCR"
 RCB_LOG_TEMP="$RCB_LOG_TEMP_RSYNC"
 
-# Optionaly dont rsync. Link the origin instead.
-if [ $LINK == "true"]; then
-    if [ -e $RCB_ENCR ]; then
-	rm -r $RCB_ENCR
-    fi
-    ln -s $RCB_ENC $RCB_ENCR
-    printf "$(date) [OK] *** Link from $RCB_ENC to $RCB_ENCR created\n" >> $RCB_LOG
-    exit 0
+printf "$(date) [OK] *** Rsync from $SRC/ to $DST (link:$LINK delete:$DELETE) started\n" >> $RCB_LOG
+
+if [ $DELETE == "true" ]; then
+    rm -r $DST
+    printf "$(date) [OK] $DST deleted\n" >> $RCB_LOG
 fi
 
-printf "$(date) [OK] *** Rsync from $SRC/ to $DST started\n" >> $RCB_LOG
+# Optionaly dont rsync. Link the origin instead.
+if [ $LINK == "true" ]; then
+    if [ -e $DST ]; then
+	rm -r $DST
+    fi
+    ln -s $RCB_ENC $DST
+    printf "$(date) [OK] *** Link from $RCB_ENC to $DST created\n" >> $RCB_LOG
+    exit 0
+else
+    if [ -L $DST ]; then
+	unlink $DST
+    fi
+    if [ ! -e $DST ]; then
+	mkdir $DST
+	printf "$(date) [OK] $DST created\n" >> $RCB_LOG
+    fi
+fi
 
 if ($RSYNC $RSYNC_PARAM -e ssh $SRC/ $DST >$RCB_LOG_TEMP 2>&1); then
     printf "$(date) [OK] *** Rsync from $SRC/ to $DST finished\n" >> $RCB_LOG
